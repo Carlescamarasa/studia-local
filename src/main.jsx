2c9c24e99d9ded86d39fbf12d156a7fe7ffebaf5
carles <carles@example.com>
2025-11-15 11:59:17 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
