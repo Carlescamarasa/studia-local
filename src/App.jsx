1045dcf59f29f4ac3dc28d07ac770d8261ac45d8
carles <carles@example.com>
2025-11-15 17:54:27 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { LocalDataProvider } from "./local-data/LocalDataProvider";
-import AppRouter from "./Router";
 import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
     <LocalDataProvider>
-      <AppRouter />
       <Toaster />
     </LocalDataProvider>
   );
